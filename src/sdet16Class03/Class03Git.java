e8197540f4fa75c222c59e69b46f7894e65d0af2
tameem904 <tameemflor904@gmail.com>
2023-04-11 23:56:53 -0400

    first commit

---
+++ src/sdet16Class03/Class03Git.java
@@ -0,0 +1,8 @@
+package sdet16Class03;
+
+public class Class03Git {
+    public static void main(String[] args) {
+        System.out.println("I am project");
+        System.out.println("I am a continuation project");
+    }
+}
